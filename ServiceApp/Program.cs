c054d32da4239664a827d047a238c429a7cf1370
Steve Hocking <steve@codemunki.es>
2018-06-22 13:44:03 +0100

    Add code to allow service to be run from cmdline
    https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/windows-service?view=aspnetcore-2.1&tabs=aspnetcore2x#provide-a-way-to-run-outside-of-a-service

--- ServiceApp/Program.cs
+++ ServiceApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
@@ -10,12 +11,33 @@ namespace ServiceApp
     {
         public static void Main(string[] args)
         {
-            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
-
-            var host = WebHost.CreateDefaultBuilder(args).UseContentRoot(pathToContentRoot).UseStartup<Startup>().Build();
-
-            host.RunAsService();
+            var isService = true;
+            
+            if (Debugger.IsAttached || args.Contains("--console"))
+            {
+                isService = false;
+            }
+            
+            var pathToContentRoot = Directory.GetCurrentDirectory();
+            
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            }
+            
+            var webHostArgs = args.Where(arg => arg != "--console").ToArray();
+            
+            var host = WebHost.CreateDefaultBuilder(webHostArgs).UseContentRoot(pathToContentRoot).UseStartup<Startup>().Build();
+            
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
     }
 }
